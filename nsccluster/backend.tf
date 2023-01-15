adee97d02d9fd5583cbb685d5a9915f1eb759c3c
Tod Hansmann <githubpub@todandlorna.com>
2023-01-14 17:13:50 -0700

    fix: ignore variable for region

--- nsccluster/backend.tf
+++ nsccluster/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "pjnpoci_tf_backend"
     key            = "terraform-oci-platform/nonprod"
-    region         = "${var.oci_region}"
+    region         = "us-phoenix-1"
     endpoint = "https://axgi7clmxnue.compat.objectstorage.us-phoenix-1.oraclecloud.com"
     # shared_credentials_file = "Set this from command line"
     skip_region_validation      = true
