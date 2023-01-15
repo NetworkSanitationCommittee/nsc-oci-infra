6da97cad14effabb809d146e732ad17f5c52d78e
Tod Hansmann <githubpub@todandlorna.com>
2023-01-14 20:20:28 -0700

    fix: format TF correctly for Ben

--- nsccluster/backend.tf
+++ nsccluster/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "pjnpoci_tf_backend"
-    key            = "terraform-oci-platform/nonprod"
-    region         = "us-phoenix-1"
-    endpoint = "https://axgi7clmxnue.compat.objectstorage.us-phoenix-1.oraclecloud.com"
+    bucket                      = "pjnpoci_tf_backend"
+    key                         = "terraform-oci-platform/nonprod"
+    region                      = "us-phoenix-1"
+    endpoint                    = "https://axgi7clmxnue.compat.objectstorage.us-phoenix-1.oraclecloud.com"
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_metadata_api_check     = true
--- nsccluster/main.tf
+++ nsccluster/main.tf
@@ -1,35 +1,35 @@
 locals {
-# Variables can not reference other variables in their default
-# This allows us to overwrite a compartment_ocid with the tenancy_ocid if the compartment_ocid
-# is not set at all
-    resolved_compartment_id = "${var.compartment_ocid}" != "" ? "${var.compartment_ocid}" : "${var.tenancy_ocid}"
+  # Variables can not reference other variables in their default
+  # This allows us to overwrite a compartment_ocid with the tenancy_ocid if the compartment_ocid
+  # is not set at all
+  resolved_compartment_id = "${var.compartment_ocid}" != "" ? "${var.compartment_ocid}" : "${var.tenancy_ocid}"
 }
 
 module "network" {
-  source   = "git::https://github.com/catalystsquad/terraform-oci-infra.git//modules/oke-network"
+  source = "git::https://github.com/catalystsquad/terraform-oci-infra.git//modules/oke-network"
 
-  compartment_id = "${local.resolved_compartment_ocid}"
+  compartment_id = local.resolved_compartment_ocid
 }
 
 module "cluster" {
-  source   = "git::https://github.com/catalystsquad/terraform-oci-infra.git//modules/oke-cluster"
+  source = "git::https://github.com/catalystsquad/terraform-oci-infra.git//modules/oke-cluster"
 
-  compartment_id = "${local.resolved_compartment_ocid}"
+  compartment_id     = local.resolved_compartment_ocid
   kubernetes_version = "1.24.1"
 
   global_freeform_tags = {
-    "env" = "everything",
-    "creator" = "Tod Hansmann",
+    "env"           = "everything",
+    "creator"       = "Tod Hansmann",
     "creator_email" = "tod@phonejanitor.com",
   }
 
-  vcn_id = module.network.vcn_id
-  nsg_id = module.network.network_security_group_id
-  public_subnet_id = module.network.subnet_ids[0]
+  vcn_id                  = module.network.vcn_id
+  nsg_id                  = module.network.network_security_group_id
+  public_subnet_id        = module.network.subnet_ids[0]
   loadbalancer_subnet_ids = [module.network.subnet_ids[0]]
-  regional_subnet_a_id = module.network.subnet_ids[2]
-  nodepool_a_subnet_ids = [module.network.subnet_ids[2]]
-  regional_subnet_b_id = module.network.subnet_ids[3]
-  nodepool_b_subnet_ids = [module.network.subnet_ids[3]]
+  regional_subnet_a_id    = module.network.subnet_ids[2]
+  nodepool_a_subnet_ids   = [module.network.subnet_ids[2]]
+  regional_subnet_b_id    = module.network.subnet_ids[3]
+  nodepool_b_subnet_ids   = [module.network.subnet_ids[3]]
 }
 
