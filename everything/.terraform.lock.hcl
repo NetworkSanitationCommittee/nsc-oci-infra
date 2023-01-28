b9c07d81df5034243148f89a8aa9e4c5b9d70ff4
Tod Hansmann <githubpub@todandlorna.com>
2023-01-28 16:48:02 -0700

    feat: everything in one TF apply, needs to be remodularized

---
+++ everything/.terraform.lock.hcl
@@ -0,0 +1,82 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/gavinbunney/kubectl" {
+  version     = "1.14.0"
+  constraints = "~> 1.14.0"
+  hashes = [
+    "h1:gLFn+RvP37sVzp9qnFCwngRjjFV649r6apjxvJ1E/SE=",
+    "zh:0350f3122ff711984bbc36f6093c1fe19043173fad5a904bce27f86afe3cc858",
+    "zh:07ca36c7aa7533e8325b38232c77c04d6ef1081cb0bac9d56e8ccd51f12f2030",
+    "zh:0c351afd91d9e994a71fe64bbd1662d0024006b3493bb61d46c23ea3e42a7cf5",
+    "zh:39f1a0aa1d589a7e815b62b5aa11041040903b061672c4cfc7de38622866cbc4",
+    "zh:428d3a321043b78e23c91a8d641f2d08d6b97f74c195c654f04d2c455e017de5",
+    "zh:4baf5b1de2dfe9968cc0f57fd4be5a741deb5b34ee0989519267697af5f3eee5",
+    "zh:6131a927f9dffa014ab5ca5364ac965fe9b19830d2bbf916a5b2865b956fdfcf",
+    "zh:c62e0c9fd052cbf68c5c2612af4f6408c61c7e37b615dc347918d2442dd05e93",
+    "zh:f0beffd7ce78f49ead612e4b1aefb7cb6a461d040428f514f4f9cc4e5698ac65",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/helm" {
+  version     = "2.8.0"
+  constraints = "~> 2.0"
+  hashes = [
+    "h1:abRryu69lsIGXctqjMVoaKqi74eE12Vzd2FLpds1/PI=",
+    "zh:1e42d1a04c07d4006844e477ca32b5f45b04f6525dbbbe00b6be6e6ec5a11c54",
+    "zh:2f87187cb48ccfb18d12e2c4332e7e822923b659e7339b954b7db78aff91529f",
+    "zh:391fe49b4d2dc07bc717248a3fc6952189cfc49c596c514ad72a29c9a9f9d575",
+    "zh:89272048e1e63f3edc3e83dfddd5a9fd4bd2a4ead104e67de1e14319294dedf1",
+    "zh:a5a057c3435a854389ce8a1d98a54aaa7cbab68aca7baa436a605897aa70ff7e",
+    "zh:b1098e53e1a8a3afcd325ecd0328662156b3d9c3d80948f19ba3a4eb870cee2b",
+    "zh:b676f949e8274a2b6c3fa41f5428ea597125579c7b93bb50bb73a5e295a7a447",
+    "zh:cdf7e9460f28c2dbfe49a79a5022bd0d474ff18120d340738aa35456ba77ebca",
+    "zh:e24b59b4ed1c593facbf8051ec58550917991e2e017f3085dac5fb902d9908cb",
+    "zh:e3b5e1f5543cac9d9031a028f1c1be4858fb80fae69f181f21e9465e366ebfa2",
+    "zh:e9fddc0bcdb28503078456f0088851d45451600d229975fd9990ee92c7489a10",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/kubernetes" {
+  version     = "2.17.0"
+  constraints = "~> 2.0"
+  hashes = [
+    "h1:I1L2R+OPgGSh+P6uBSycvvoyRIey/FqMwSvlJ9ccw0o=",
+    "zh:1cbafea8c404195d8ad2490d75dbeebef131563d3e38dec87231ceb3923a3012",
+    "zh:26d9584423ee77e607999b082de7d9dc3e937934aa83341e0832e7253caf4f51",
+    "zh:333527fc15fb43bbf1898a2f058598c596468a01d88c415627bb617878dc4d4d",
+    "zh:391b8c80e3115af485977d6e949d7260b7fc0b641089b884256bfd36a7077db2",
+    "zh:4d18ba55247486181759d60195777945bcd68e17ccd980820ca18e8a8b94aeb5",
+    "zh:607ae94d85d1c1ed3845bd71095daadea4b2468e16f57fa05c98eab0de6b14ae",
+    "zh:95c6cf22f8ef14e7a4f85e33cff5d6f11056c7880041b71d425d1b5ebbe246e7",
+    "zh:b077edcedb46a313b461ac1e49317872063b3871f2acbe1a50498612cefff387",
+    "zh:c6a7891683e44148b0c928fd4748b7abac727266ab551d679015f5fe8b72d1e6",
+    "zh:e5cebfdf873770c37a4304362003d3fea8d6c2fd819663ad121bc65bb81e4738",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+    "zh:feb19269e7c0de473ad412b37818b48da0cc91e5c93dd4c77a72676ca97a16b1",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/oci" {
+  version     = "4.104.2"
+  constraints = ">= 4.0.0"
+  hashes = [
+    "h1:ywBpZi2M9xdqDemHZ0sBBBB+uVjnOA217rAh8VCoNlQ=",
+    "zh:3074657bd240bd435226dbe8a2c267b97dc6a53167cbc7a266a7e762062cd002",
+    "zh:470246b12e4fa945ef09e9042d767ea3db1e5fde7ee984a63b2c602d681c3ab5",
+    "zh:49f0b051f5f39dd5735dc24956178e531023c88f7452dd8232935721772e6bee",
+    "zh:4a7db067fc9e580daf0b90a04a71d234a44cb81686eb8a5ecd6c3480ba0a14b6",
+    "zh:53d95134eb02a8bf90eaf2b897c9b68ff7fdbee4e3bbf291afbc29600e20fc88",
+    "zh:68c2fe3568c388c72da033c8ef82d501d06d84a604dcb79ec2b586cb6f6f7b57",
+    "zh:784940c0afb5c5d4dc2b93bdda5ca28f5fced5744ebae4e779c5ba2c4bbd9764",
+    "zh:7a0ee22a019e3a867025bfe855506f0ea7addcba979c245c9e3dd3c469521935",
+    "zh:7d268b06c9b7e8f7a520375f624542d2c967615f77e99f5689883ef65e45c3d5",
+    "zh:9a4ac37a9f716ace939a77585f341e01fce04c24af748b4133f68780107d97c7",
+    "zh:9b12af85486a96aedd8d7984b0ff811a4b42e3d88dad1a3fb4c0b580d04fa425",
+    "zh:bb71306497c9fd88ad898a7ce2aba598a0de19f7bc9ece7472fbbeee41b3f442",
+    "zh:ef8815641af0bad577af1d10d602a3d81ad6352af3117fe2a70c53f60808b2ec",
+    "zh:f046b1c1e06dfd9a5435ea506dd7d141d255415c3b8b56cb2871197c99f2b817",
+    "zh:fb661bf63ac6517cacc4eda7f43792f900b52608c69cb3f808ddb487c1cfae29",
+  ]
+}
--- everything/main.tf
+++ everything/main.tf
@@ -6,60 +6,125 @@ locals {
 }
 
 module "network" {
-  source = "git::https://github.com/catalystsquad/terraform-oci-infra.git//modules/oke-network"
+  #source = "git::https://github.com/catalystsquad/terraform-oci-infra.git//modules/oke-network"
+  source = "../modules/oke-network"
 
   compartment_id = local.resolved_compartment_ocid
 }
 
 module "cluster" {
-  source = "git::https://github.com/catalystsquad/terraform-oci-infra.git//modules/oke-cluster"
+  #source = "git::https://github.com/catalystsquad/terraform-oci-infra.git//modules/oke-cluster"
+  source = "../modules/oke-cluster"
 
   compartment_id     = local.resolved_compartment_ocid
   kubernetes_version = "1.25.4"
 
-  global_freeform_tags = {
-    "env"           = "everything",
-    "creator"       = "Tod Hansmann",
-    "creator_email" = "tod@phonejanitor.com",
-  }
-
+  global_freeform_tags    = var.global_freeform_tags
   vcn_id                  = module.network.vcn_id
   nsg_id                  = module.network.network_security_group_id
-  public_subnet_id        = module.network.subnet_ids[0]
-  loadbalancer_subnet_ids = [module.network.subnet_ids[0]]
-  regional_subnet_a_id    = module.network.subnet_ids[2]
-  nodepool_a_subnet_ids   = [module.network.subnet_ids[2]]
-  regional_subnet_b_id    = module.network.subnet_ids[3]
-  nodepool_b_subnet_ids   = [module.network.subnet_ids[3]]
+  public_subnet_id        = module.network.public_regional_subnet_id
+  loadbalancer_subnet_ids = [module.network.public_subnet_a_id, module.network.public_subnet_b_id]
+  service_subnet_cidr     = "10.21.0.0/16"
+
+  #regional_subnet_a_id  = module.network.private_subnet_ids[0]
+  #nodepool_a_subnet_ids = [module.network.private_subnet_ids[0]]
+  #regional_subnet_b_id  = module.network.private_subnet_ids[1]
+  #nodepool_b_subnet_ids = [module.network.private_subnet_ids[1]]
+  #public_subnet_a_id    = module.network.public_subnet_a
+  nodepool_a_subnet_ids = [module.network.private_subnet_a_id]
+  #public_subnet_b_id    = module.network.public_subnet_b
+  nodepool_b_subnet_ids = [module.network.private_subnet_b_id]
 }
 
-resource "oci_objectstorage_bucket" "metrics-bucket" {
+resource "oci_objectstorage_bucket" "platform_metrics_bucket" {
   compartment_id = local.resolved_compartment_ocid
-  name           = "nsc-metrics-bucket"
+  name           = "${var.infra_set_name}_catalyst_metrics_bucket"
   namespace      = "axgi7clmxnue"
 }
 
-locals {
-  environment_name = "everything"
+resource "oci_identity_user" "platform_metrics_bucket_user" {
+  compartment_id = local.resolved_compartment_ocid
+  name           = "${var.infra_set_name}_catalyst_metrics_bucket_user"
+  email          = "tod+${var.infra_set_name}@phonejanitor.com"
+  description    = "User to access the ${var.infra_set_name}_catalyst_metrics_bucket_user"
+  freeform_tags  = merge(var.global_freeform_tags, {})
 }
 
-# manage secrets for this environment via aws secrets manager for secret
-# versioning and aws access control over secrets
+resource "oci_identity_group" "platform_metrics_users" {
+  compartment_id = local.resolved_compartment_ocid
+  description    = "Group for managing access to ${var.infra_set_name}_catalyst_metrics_bucket"
+  name           = "${var.infra_set_name}_metrics_users"
+
+  freeform_tags = merge(var.global_freeform_tags, {})
+}
+
+resource "oci_identity_policy" "platform_metrics_bucket_user_policy" {
+  compartment_id = local.resolved_compartment_ocid
+  description    = "Allow the metrics bucket user have access to the metrics bucket of th ${var.infra_set_name} stack."
+  name           = "${var.infra_set_name}_catalyst_metrics_bucket_user_policy"
+  statements = [
+    "Allow group ${var.infra_set_name}_metrics_users to manage buckets in tenancy where all {target.bucket.name='{var.infra_set_name}_catalyst_metrics_bucket'}",
+    "Allow group ${var.infra_set_name}_metrics_users to manage objects in tenancy where all {target.bucket.name='{var.infra_set_name}_catalyst_metrics_bucket'}",
+  ]
+
+  freeform_tags = merge(var.global_freeform_tags, {})
+}
+
+resource "oci_identity_user_group_membership" "platform_metrics_bucket_users_group_membership" {
+  group_id = oci_identity_group.platform_metrics_users.id
+  user_id  = oci_identity_user.platform_metrics_bucket_user.id
+}
+
+resource "oci_identity_customer_secret_key" "platform_metrics_bucket_user_key" {
+  display_name = "Keys for the ${var.infra_set_name}_catalyst_metrics_bucket_user"
+  user_id      = oci_identity_user.platform_metrics_bucket_user.id
+}
+
+locals {
+  metrics_bucket_namespaces = [
+    "cortex",
+    "loki",
+  ]
+}
+
+# manage secrets for this environment via oci secrets manager for secret
+# versioning and oci access control over secrets
 data "oci_secrets_secretbundle" "platform_secrets" {
   secret_id = var.platform_secret_ocid
 }
 
 locals {
-  secrets = jsondecode(
-    base64decode(
-      data.oci_secrets_secretbundle.platform_secrets.secret_bundle_content.0.content
+  environment_name = "everything"
+  secrets = sensitive(
+    jsondecode(
+      base64decode(
+        data.oci_secrets_secretbundle.platform_secrets.secret_bundle_content.0.content
+      )
     )
   )
 }
 
+resource "kubernetes_storage_class" "platform_storage_class_paravirtualized" {
+  metadata {
+    name = "oci-bv-enc"
+    annotations = {
+      "storageclass.kubernetes.io/is-default-class" = "true"
+    }
+  }
+  storage_provisioner = "blockvolume.csi.oraclecloud.com"
+  reclaim_policy      = "Retain"
+  parameters = {
+    attachment-type = "paravirtualized"
+  }
+  allow_volume_expansion = true
+  volume_binding_mode    = "WaitForFirstConsumer"
+}
+
 module "bootstrap" {
-  source  = "catalystsquad/catalyst-cluster-bootstrap/kubernetes"
-  version = "~> 1.0"
+  count      = 1
+  depends_on = [module.cluster, kubernetes_storage_class.platform_storage_class_paravirtualized]
+  source     = "catalystsquad/catalyst-cluster-bootstrap/kubernetes"
+  version    = "~> 1.0"
 
   enable_platform_services          = true
   cert_manager_cloudflare_api_token = local.secrets.cloudflareApiToken
@@ -73,12 +138,15 @@ module "bootstrap" {
 
   argo_cd_values = [
     templatefile("./helm-values/argocd.yaml", {
-      #"helmRepoPat" : local.secrets.helmRepoPat,
-      "helmRepoPat" : "NotRealPAT",
+      "helmRepoPat" : local.secrets.helmRepoPat,
     })
   ]
 
+
   platform_services_values = templatefile("./helm-values/${local.environment_name}-platform-services.yaml", {
+    "metricsBucketName" : "${var.infra_set_name}_catalyst_metrics_bucket"
+    "metricsAwsAccessKeyId" : oci_identity_customer_secret_key.platform_metrics_bucket_user_key.id,
+    "metricsAwsSecretAccessKey" : oci_identity_customer_secret_key.platform_metrics_bucket_user_key.key,
     "cloudflareApiToken" : local.secrets.cloudflareApiToken,
     "grafanaDatasourceCortexPassword" : local.secrets.grafanaDatasourceCortexPassword,
     "grafanaDatasourceLokiPassword" : local.secrets.grafanaDatasourceLokiPassword,
@@ -96,3 +164,25 @@ module "bootstrap" {
     "linkerdIssuerKeyPEM" : indent(12, base64decode(local.secrets.linkerdIssuerKeyPEMb64)),
   })
 }
+
+resource "kubernetes_secret_v1" "prometheus_remote_write" {
+  for_each = toset(local.metrics_bucket_namespaces)
+
+  metadata {
+    name      = "metricsbucketaccess"
+    namespace = each.key
+  }
+
+  data = {
+    #AWS_ACCESS_KEY_ID
+    access_key_id = oci_identity_customer_secret_key.platform_metrics_bucket_user_key.id
+    #AWS_SECRET_ACCESS_KEY 
+    secret_access_key = oci_identity_customer_secret_key.platform_metrics_bucket_user_key.key
+  }
+
+  depends_on = [
+    module.cluster,
+    module.bootstrap,
+  ]
+}
+
